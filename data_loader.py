612c5bfab4806a16205f1f4980c4c3ef2a16a99a
RainonSunny <bubble60324@gmail.com>
2024-08-09 23:54:22 +0800

    first-commit

---
+++ data_loader.py
@@ -0,0 +1,105 @@
+from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import imageio
+from PIL import Image
+
+class DataLoader():
+    def __init__(self, img_res):
+        #(128,128)
+        self.img_res = img_res
+
+    def load_data(self, batch_size=1, is_testing=False):
+        
+        if not is_testing:
+            class_type = "Train"
+        else:
+            class_type = "Test"
+            
+
+        path = './Dataset/%s/Reflection/' % (class_type)
+        path1 = glob('./Dataset/%s/Reflection/*' % (class_type))
+        path2 = './Dataset/%s/NonReflection/' % (class_type)
+
+        temp_len = len(path)
+
+        batch_images = np.random.choice(path1, size=batch_size)
+
+        batch_images1 = list()
+        
+        
+        imgs_A = []
+        imgs_B = []
+        
+        for img_path in batch_images:
+            img = self.imread(img_path)
+            batch_images1.append(path2 + img_path[temp_len:])
+            img = np.array(Image.fromarray(np.uint8(img)).resize(self.img_res))
+
+            # If training => do random flip
+            if not is_testing and np.random.random() < 0.5:
+                img = np.fliplr(img)
+                
+                
+            imgs_A.append(img)
+
+        imgs_A = np.array(imgs_A)/127.5 - 1.# normalize
+            
+        for img_path in batch_images1:
+            img = self.imread(img_path)
+            
+            img = np.array(Image.fromarray(np.uint8(img)).resize(self.img_res))
+
+            # If training => do random flip
+            if not is_testing and np.random.random() < 0.5:
+                img = np.fliplr(img)
+            
+            imgs_B.append(img)
+
+        imgs_B = np.array(imgs_B)/127.5 - 1. # normalize
+
+        return imgs_A, imgs_B
+
+    def load_batch(self, batch_size=1, is_testing=False):
+        
+        if not is_testing:
+            class_type = "Train"
+        else:
+            class_type = "Test"
+            
+
+        path = './Dataset/%s/Reflection/' % (class_type)
+        path1 = glob('./Dataset/%s/Reflection/*' % (class_type))
+        path2 = './Dataset/%s/NonReflection/' % (class_type)
+
+        self.n_batches = int(len(path1) / batch_size)
+
+        temp_len = len(path)
+        for i in range(self.n_batches-1):
+            batch = path1[i*batch_size:(i+1)*batch_size]
+            imgs_A, imgs_B = [], []
+            for path_A in batch:
+
+                path_B = path2 + path_A[temp_len:]
+                img_A = self.imread(path_A)
+                img_B = self.imread(path_B)
+
+                img_A = np.array(Image.fromarray(np.uint8(img_A)).resize(self.img_res))
+                img_B = np.array(Image.fromarray(np.uint8(img_B)).resize(self.img_res))
+
+                if not is_testing and np.random.random() > 0.5:
+                        img_A = np.fliplr(img_A)
+                        img_B = np.fliplr(img_B)
+                
+                imgs_A.append(img_A)
+                imgs_B.append(img_B)
+
+            imgs_A = np.array(imgs_A)/127.5 - 1.# normalize
+            imgs_B = np.array(imgs_B)/127.5 - 1.# normalize
+
+
+            yield imgs_A, imgs_B
+
+
+    def imread(self, path):
+        return imageio.imread(path, mode='RGB').astype(np.float)
